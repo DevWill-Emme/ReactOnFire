84d8f1f092090a05524d995cca9847c6049d35a2
DevWill-Emme <musteliergarridojavier@gmail.com>
2022-04-10 01:53:09 -0400

    Setting Titles

--- src/pages/Register/index.jsx
+++ src/pages/Register/index.jsx
@@ -1,5 +1,5 @@
 import './style.css';
-import {Button, Card, Container, Form, Row} from "react-bootstrap";
+import {Button, Card, Col, Container, Form, Row} from "react-bootstrap";
 import {useState} from "react";
 
 function SignUp() {
@@ -15,51 +15,43 @@ function SignUp() {
 	};
 
 	return (
-		<Container fluid className="d-flex justify-content-center position-fixed top-50 translate-middle-y">
-			<Card
-				className="p-4 shadow"
-				style={{borderRadius: "1.5rem"}}>
-				<Form
-					noValidate
-					validated={validated}
-					onSubmit={handleSubmit}>
-					<Row className={"mb-3"}>
-						<Form.Group>
-							<Form.Label>Email Address</Form.Label>
-							<Form.Control
-								type={"email"}
-								placeholder="Email Address"
-								required/>
-							<Form.Control.Feedback
-								type="invalid">
-								Please provide a email address valid
-							</Form.Control.Feedback>
-						</Form.Group>
-					</Row>
-					<Row>
-						<Form.Group>
-							<Form.Label>Password</Form.Label>
-							<Form.Control
-								type="password"
-								placeholder="Password"
-								required/>
-							<Form.Control.Feedback
-								type={"invalid"}>
-								password's not valid
-							</Form.Control.Feedback>
-						</Form.Group>
-					</Row>
-					<Row className={"d-flex justify-content-center"}>
-						<Button
-							className="mt-3 w-50"
-							type={"submit"}
-							variant={"primary"}
-						>
-							Sign Up
-						</Button>
-					</Row>
-				</Form>
-			</Card>
+		<Container fluid className="d-flex justify-content-center">
+			<Row className="position-fixed top-50 translate-middle-y">
+				<Card className="w-auto p-4 shadow" style={{borderRadius: "1.5rem"}}>
+					<Form noValidate validated={validated} onSubmit={handleSubmit}>
+						<Row className={"text-center m-3"}>
+							<h1>Sign Up</h1>
+						</Row>
+						<Row className="mb-3">
+							<Form.Group as={Col}>
+								<Form.Label>Email Address</Form.Label>
+								<Form.Control
+									type="email"
+									placeholder="Email Address"
+									required
+								/>
+								<Form.Control.Feedback type="invalid">
+									Please provide a valid email address
+								</Form.Control.Feedback>
+							</Form.Group>
+						</Row>
+						<Row>
+							<Form.Group as={Col}>
+								<Form.Label>Password</Form.Label>
+								<Form.Control
+									required
+									type="password"
+									placeholder="Password"
+								/>
+								<Form.Control.Feedback type="invalid">Password's invalid</Form.Control.Feedback>
+							</Form.Group>
+						</Row>
+						<Row className="d-flex justify-content-center">
+							<Button className="mt-3 w-50" type="submit">Sign Up</Button>
+						</Row>
+					</Form>
+				</Card>
+			</Row>
 		</Container>
 	);
 }
