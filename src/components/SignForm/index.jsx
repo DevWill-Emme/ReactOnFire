85fafdd397b5bb59b0d513cdca3ff5907b1ef5d3
DevWill-Emme <musteliergarridojavier@gmail.com>
2022-04-10 11:03:50 -0400

    ehhh muchos cambios?? xD SignForm setting title & btn

--- src/components/SignForm/index.jsx
+++ src/components/SignForm/index.jsx
@@ -1,8 +1,8 @@
 import {useState} from "react";
-import {Card, Form, Row} from "react-bootstrap";
+import {Button, Card, Form, Row} from "react-bootstrap";
 
 
-export default function SForm() {
+export default function SForm({children, title = "hello", render = [], btnValue = "Submit"}) {
 
 	const [validated, setValidated] = useState(false);
 
@@ -14,23 +14,32 @@ export default function SForm() {
 		}
 		setValidated(true);
 	};
-
-
+	console.log(render)
 	const renderRows = () => {
+
 		return (
 			<Row className={"d-flex justify-content-center"}>
-				<h1 className={"w-auto"}>Hello World!!!</h1>
+				<Form.Label defaultValue={"Test"}/>
 			</Row>
 		)
 	}
-
+	const center = "d-flex justify-content-center"
 
 	return (
 		<>
-			<Card className="p-4 shadow" style={{borderRadius: "1.5rem"}}>
-				<Form noValidate validated={validated} onSubmit={handleSubmit}>
-					{renderRows()}
-				</Form>
+			<Card className={`p-4 shadow ${center}`} style={{borderRadius: "1.5rem"}}>
+				<Row className={center}>
+					<h1 className={"w-auto"}>{title}</h1>
+				</Row>
+				<Row className={center}>
+					<Form noValidate validated={validated} onSubmit={handleSubmit}>
+						{renderRows()}
+						<Row className={center}>
+							<Button className={"btn w-auto"} type="submit">{btnValue}</Button>
+						</Row>
+					</Form>
+				</Row>
+				<Row>{children}</Row>
 			</Card>
 		</>
 	);
