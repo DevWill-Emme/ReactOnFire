163d39ed11d68b4a246f2f0158bc81b562a3101f
DevWill-Emme <musteliergarridojavier@gmail.com>
2022-04-11 13:51:59 -0400

    fix connected SigUp with firebase Auth Service

--- src/actions/hendleAuth.jsx
+++ src/actions/hendleAuth.jsx
@@ -4,13 +4,42 @@ const handleAuth = ({email, pass, action}) => {
 	const auth = getAuth()
 	
 	if (action === 'createUser') {
-		createUserWithEmailAndPassword(auth, email, pass).then(cred => {
-			console.log(cred)
+		createUserWithEmailAndPassword(auth, email, pass)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			// ...
 		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+			// ..
+		});
 	} else {
-		//signInWithEmailAndPassword(auth, email, pass)
+		/*signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			// Signed in
+			const user = userCredential.user;
+			// ...
+		})
+		.catch((error) => {
+			const errorCode = error.code;
+			const errorMessage = error.message;
+		});*/
 	}
 }
+
+/*onAuthStateChanged(auth, (user) => {
+	if (user) {
+		// User is signed in, see docs for a list of available properties
+		// https://firebase.google.com/docs/reference/js/firebase.User
+		const uid = user.uid;
+		// ...
+	} else {
+		// User is signed out
+		// ...
+	}
+});*/
 
 export default handleAuth;
 
