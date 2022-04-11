c82d2a2e3fd019705b81a336311b4479ed383ee4
DevWill-Emme <musteliergarridojavier@gmail.com>
2022-04-11 13:18:48 -0400

    connected SigUp with firebase Auth Service

--- src/index.jsx
+++ src/index.jsx
@@ -13,24 +13,26 @@ import Test from "./pages/Test";
 import ThemeProvider from "react-bootstrap/ThemeProvider";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+//initialize Firebase
+
 ReactDOM
-	.createRoot(document.getElementById('root'))
-	.render(
-		<React.StrictMode>
-			<BrowserRouter>
-				<ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
-					<Index>
-						<Routes>
-							<Route path="/" element={<Main/>}/>
-							<Route path="login" element={<Login/>}/>
-							<Route path="signup" element={<SignUp/>}/>
-							<Route path="test" element={<Test/>}/>
-						</Routes>
-					</Index>
-				</ThemeProvider>
-			</BrowserRouter>
-		</React.StrictMode>
-	);
+.createRoot(document.getElementById('root'))
+.render(
+	<React.StrictMode>
+		<BrowserRouter>
+			<ThemeProvider breakpoints={['xxxl', 'xxl', 'xl', 'lg', 'md', 'sm', 'xs', 'xxs']}>
+				<Index>
+					<Routes>
+						<Route path="/" element={<Main/>}/>
+						<Route path="login" element={<Login/>}/>
+						<Route path="signup" element={<SignUp/>}/>
+						<Route path="test" element={<Test/>}/>
+					</Routes>
+				</Index>
+			</ThemeProvider>
+		</BrowserRouter>
+	</React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
